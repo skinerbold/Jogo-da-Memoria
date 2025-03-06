9002da44b6269dc3b1b1b0ad477b598d6bfcfb3e
skinerbold <134705374+skinerbold@users.noreply.github.com>
2025-03-06 18:19:45 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css';
 
 export const metadata = {
-  title: 'Jogo da Memória',
-  description: 'Jogo da Memória com Vídeos',
+  title: 'Jogo da Memória com Libras',
+  description: 'Aprenda Libras enquanto se diverte com este jogo da memória interativo',
 }
 
 export default function RootLayout({
@@ -13,11 +13,15 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#8d334b" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
-      <body className="min-h-screen antialiased font-poppins">
+      <body className="min-h-screen antialiased font-poppins overflow-x-hidden">
         {children}
       </body>
     </html>
